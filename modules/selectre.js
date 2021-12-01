ca6f3e051633910b07d79c41d4ea08c5567a134d
Alexey Raspopov <alexeyraspopov@hotmail.com>
2021-11-30 23:01:13 -0500

    reduce selector size

--- modules/selectre.js
+++ modules/selectre.js
@@ -1,6 +1,7 @@
 import { LRUCache } from "./LRUCache.js";
 import { hashCode } from "./hashCode.js";
 import { shallowEqual } from "./shallowEqual.js";
+import { applier } from "./apply.js";
 
 let defaults = {
   capacity: 256,
@@ -10,7 +11,6 @@ let defaults = {
 };
 
 let slice = Array.prototype.slice;
-let applyFns = {};
 
 export function createSelector(/* ...inputs, ouput, options */) {
   let options, inputs, output;
@@ -27,30 +27,7 @@ export function createSelector(/* ...inputs, ouput, options */) {
   }
 
   let cache = LRUCache(options.capacity);
-
-  function outputSelectorAccessor(/* ...params */) {
-    let params = arguments;
-    let key = options.cacheKey.apply(null, params);
-
-    if (cache.has(key)) {
-      let entry = cache.get(key);
-      return entry.selector;
-    }
-
-    let entry = { result: null, selector: null };
-    function selector(target) {
-      let inputParams = params.length > 0 ? [target].concat(Array.from(params)) : [target];
-      let result = output.apply(null, inputParams);
-      if (!options.isOutputEqual(result, entry.result)) {
-        entry.result = result;
-      }
-      return entry.result;
-    }
-    entry.selector = selector;
-    cache.set(key, entry);
-
-    return entry.selector;
-  }
+  let select = inputs.length > 0 ? selectWithInputs : selectOutput;
 
   function selectorAccessor(/* ...params */) {
     let params = arguments;
@@ -61,38 +38,42 @@ export function createSelector(/* ...inputs, ouput, options */) {
       return entry.selector;
     }
 
-    let apply =
-      applyFns[params.length] ||
-      (applyFns[params.length] = new Function(
-        "f",
-        "t",
-        "p",
-        `let i=0; return f(t${", p[i++]".repeat(params.length)});`,
-      ));
-
-    let entry = { inputs: new Array(inputs.length), result: null, selector: null };
-    function selector(target) {
-      let dirty = false;
-      for (let i = 0; i < inputs.length; i++) {
-        let inputValue = apply(inputs[i], target, params);
-        dirty = dirty || !options.isInputEqual(inputValue, entry.inputs[i]);
-        entry.inputs[i] = inputValue;
-      }
-      if (dirty) {
-        let newResult = output.apply(null, entry.inputs);
-        if (!options.isOutputEqual(newResult, entry.result)) {
-          entry.result = newResult;
-        }
-      }
-      return entry.result;
-    }
-    entry.selector = selector;
+    let apply = applier(params.length);
+    let entry = {
+      inputs: new Array(inputs.length),
+      result: null,
+      selector: (target) => select(entry, inputs, output, target, params, options, apply),
+    };
     cache.set(key, entry);
 
     return entry.selector;
   }
 
-  return inputs.length > 0 ? selectorAccessor : outputSelectorAccessor;
+  return selectorAccessor;
+}
+
+function selectWithInputs(entry, inputs, output, target, params, options, apply) {
+  let dirty = false;
+  for (let i = 0; i < inputs.length; i++) {
+    let inputValue = apply(inputs[i], target, params);
+    dirty = dirty || !options.isInputEqual(inputValue, entry.inputs[i]);
+    entry.inputs[i] = inputValue;
+  }
+  if (dirty) {
+    let newResult = output.apply(null, entry.inputs);
+    if (!options.isOutputEqual(newResult, entry.result)) {
+      entry.result = newResult;
+    }
+  }
+  return entry.result;
+}
+
+function selectOutput(entry, inputs, output, target, params, options, apply) {
+  let result = apply(output, target, params);
+  if (!options.isOutputEqual(result, entry.result)) {
+    entry.result = result;
+  }
+  return entry.result;
 }
 
 function cacheKey() {
